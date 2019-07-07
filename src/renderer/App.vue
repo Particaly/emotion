ad3a1ecfcb26261de2a0605d8dc8c60933374fde
JSPatrick <980141374@qq.com>
2019-07-07 17:29:51 +0800

    emotions

---
+++ src/renderer/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'emotion',
+  }
+</script>
+
+<style>
+  /* CSS */
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html,body,#app{
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container"></div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        mounted() {
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+.container{
+    width: 100%;
+    height: 100%;
+    background: rgba(10,10,10,0.7);
+}
+</style>
